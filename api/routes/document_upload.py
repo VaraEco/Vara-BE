ba73b997bfce034dc54d66ea4b3c90dad5972361
Shraddha  Agrawal <shraddhaagrawal@Shraddhas-MacBook-Pro.local>
2024-08-04 19:15:15 -0700

    modified upload api

--- api/routes/document_upload.py
+++ api/routes/document_upload.py
@@ -4,7 +4,7 @@ import logging
 import boto3
 from botocore.exceptions import ClientError
 import json
-from io import BytesIO
+from services.textract_services import Textract
 
 document_upload_bp = Blueprint('document_upload', __name__)
 CORS(document_upload_bp, resources={r"/upload/document/analysis":{"origins":"*"}}, methods=['POST'])
@@ -13,25 +13,26 @@ logger = logging.getLogger('vara-backend')
 @document_upload_bp.route('/upload/document/analysis', methods=['POST'])
 def upload_document():
     bucket_name = "compliance-document-bucket"
-    data = None
-    if request.content_type == 'application/octet-stream':
-        byte_array = request.data
-        try:
-            byte_string = byte_array.decode('utf-8')
-            data = json.loads(byte_string)
-        except (UnicodeDecodeError, json.JSONDecodeError) as e:
-                return jsonify({'error': str(e)}), 400
-    else:
-        data = request.get_json()
-    file_name = data['fileName']
-    file = data['file']
-    temp_file = BytesIO()
-    temp_file.write(file.encode('utf-8'))
-    temp_file.seek(0)
+
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file part'}), 400
+
+    file = request.files.get('file', '')
+
+    fileName = str(request.form.get('fileName', ''))
+    print(fileName)
+
+    # Upload the file to S3
     s3_client = boto3.client('s3')
     response = True
     try:
-        response = s3_client.upload_file(temp_file, bucket_name, file_name)
+        s3_client.upload_fileobj(file, bucket_name, fileName)
     except ClientError as e:
-         logger.error(e)
-    return jsonify({'upload':response})
+        logger.error(e)
+
+    # Add code for getting value from TextRact
+    textract = Textract()
+    response = textract.get_analysis(bucket_name, fileName)
+    print(response)
+
+    return jsonify(response)
